5978cd51c1f17d4f4d69d893a9c3ea72a119f4c3
LudoCarlu <ludoviccarlu@gmail.com>
2018-11-26 13:58:00 +0100

    [Liste] : Affichage de la liste avec le web service

--- app/src/main/java/com/github/ludoviccarlu/pokemon/data/remote/RestApiService.kt
+++ app/src/main/java/com/github/ludoviccarlu/pokemon/data/remote/RestApiService.kt
@@ -1,9 +1,18 @@
 package com.github.ludoviccarlu.pokemon.data.remote
 
+import com.github.ludoviccarlu.pokemon.data.pojo.RestPokemonData
+import com.github.ludoviccarlu.pokemon.domain.Pokemon
+import io.reactivex.Single
+import retrofit2.Response
+import retrofit2.http.GET
+import java.util.*
+
 /**
  * Created by ludoviccarlu on 19/11/2018.
  */
 
 
 interface RestApiService {
+    @GET("/api/v2/pokemon")
+    fun getListPokemon() : Single<Response<RestPokemonData>>
 }
